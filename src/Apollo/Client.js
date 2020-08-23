6d1cc53bd6b4e10fa9bad29ae14247c55a1d32be
조병근 <airmancho@naver.com>
2020-08-23 19:28:50 +0900

    08/23

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -3,7 +3,7 @@ import  ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: process.env.NODE_ENV === "development" ? 'http://localhost:4000' : "https://packeasydb-5920ab540d.herokuapp.com",
+  uri: process.env.NODE_ENV === "development" ? 'http://localhost:4000' : "https://packeasydb-5920ab540d.herokuapp.com/packeasy/prod",
   clientState: {
     defaults,
     resolvers
